876706dfa36a71a21cc33f3376ef8b8bbf831117
Karam Santh <thekaramsanth@gmail.com>
2026-07-13 04:42:18 +0000

    Initial commit

---
+++ twoSum.py
@@ -0,0 +1,24 @@
+def twoSum(arr,n):
+    left = 0
+    right = len(arr) -1
+
+    while left < right:
+        target = arr[left] + arr[right]
+
+        if target == n:
+            return [arr[left],arr[right]]
+        elif target< sum:
+            left+=1
+        else:
+            right-=
+    return []
+
+def main():
+    arr = [1,2,3,4,5]
+    n = 8
+
+    print(twoSum(arr,n))
+
+if __name__ == "__main__":
+    main()
+
